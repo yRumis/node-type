173acc414735a0db898f7b77acf9b6f6971c9209
ramon <ramon.silva109@.com>
2026-05-10 19:43:12 -0300

    feat: mongoclient

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,33 @@ import express from 'express'
 import { config } from 'dotenv'
 import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users.js';
 import { GetUsersController } from './controllers/get-users/get-users.js';
+import { MongoClient } from './database/mongo.js';
 
-config();
 
-const app = express()
-const port = process.env.PORT
 
-app.get('/users', async (req,res)=>{
-    console.log("estamos no get")
+const main = async () => {
+  config();
+
+  const app = express();
+
+  await MongoClient.connect();
+
+  app.get("/users", async (req, res) => {
+    console.log("estamos no get");
     const mongoRepository = new MongoGetUsersRepository();
 
     const getUsersController = new GetUsersController(mongoRepository);
 
-    const {body, statusCode} = await getUsersController.handle();
+    const { body, statusCode } = await getUsersController.handle();
 
     res.send(body).status(statusCode);
+  });
 
-})
+  const port = process.env.PORT;
 
+  app.listen(port, () => {
+    console.log("servidor de pe");
+  });
+};
 
-
-app.listen(port,()=>{
-    console.log("servidor de pe")
-})
+main();
